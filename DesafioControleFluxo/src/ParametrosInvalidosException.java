7c0fc33d2f31baa0de0183ba3bf2e7e29c68cf92
cicerorda <cicero.rda@gmail.com>
2024-09-21 21:36:19 -0300

    entrega projeto

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+public class ParametrosInvalidosException extends Exception {
+    
+    // Construtor que recebe a mensagem da exceção
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);  // Chama o construtor da classe Exception, passando a mensagem
+    }
+}
